26564181521cf7c76cd5c2c2fd53ab7701596ee4
Luke Sheppard <sheppiegit@gmail.com>
2023-07-18 16:57:30 -0400

    Have a simple box contract taht the dao will own.

--- src/Box.sol
+++ src/Box.sol
@@ -1,4 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
 
-contract Box {}
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Box is Ownable {
+    uint256 private number;
+
+    event NumberChanged(uint256 number);
+
+    function store(uint256 newNumber) public onlyOwner {
+        number = newNumber;
+        emit NumberChanged(newNumber);
+    }
+
+    function getNumber() external view returns (uint256) {
+        return number;
+    }
+}
