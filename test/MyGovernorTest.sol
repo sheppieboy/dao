1ba4554d2ee75afc515b15ecd739ea8412da57ef
Luke Sheppard <sheppiegit@gmail.com>
2023-07-18 21:34:09 -0400

    finished initial setup of contracts.

--- test/MyGovernorTest.sol
+++ test/MyGovernorTest.sol
@@ -7,4 +7,39 @@ import "../src/Box.sol";
 import "../src/GovToken.sol";
 import "../src/TimeLock.sol";
 
-contract MyGovernorTest is Test {}
+contract MyGovernorTest is Test {
+    MyGovernor governor;
+    Box box;
+    TimeLock timelock;
+    GovToken govToken;
+
+    address public USER = makeAddr("user");
+    uint256 public constant INITIAL_SUPPLY = 100 ether;
+    uint256 public constant MIN_DELAY = 3600; //1 hour;
+
+    address[] proposers;
+    address[] executors;
+
+    function setUp() public {
+        govToken = new GovToken();
+        govToken.mint(USER, INITIAL_SUPPLY);
+
+        vm.startPrank(USER);
+        govToken.delegate(USER);
+        timelock = new TimeLock(MIN_DELAY, proposers, executors);
+        governor = new MyGovernor(govToken, timelock);
+
+        bytes32 proposerRole = timelock.PROPOSER_ROLE();
+        bytes32 executorRole = timelock.EXECUTOR_ROLE();
+        bytes32 adminRole = timelock.TIMELOCK_ADMIN_ROLE();
+
+        timelock.grantRole(proposerRole, address(governor));
+
+        timelock.grantRole(executorRole, address(0)); //anyone can execute
+        timelock.revokeRole(adminRole, USER);
+        vm.stopPrank();
+
+        box = new Box();
+        box.transferOwnership(address(timelock));
+    }
+}
