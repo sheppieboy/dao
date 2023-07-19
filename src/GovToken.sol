9c7be47bc3359f839286ae7df48bb25f11e24f73
Luke Sheppard <sheppiegit@gmail.com>
2023-07-18 21:28:21 -0400

    changed contract name and symbols to govtoken

--- src/GovToken.sol
+++ src/GovToken.sol
@@ -5,8 +5,8 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 
-contract MyToken is ERC20, ERC20Permit, ERC20Votes {
-    constructor() ERC20("MyToken", "MTK") ERC20Permit("MyToken") {}
+contract GovToken is ERC20, ERC20Permit, ERC20Votes {
+    constructor() ERC20("GovToken", "GVT") ERC20Permit("GovToken") {}
 
     // The following functions are overrides required by Solidity.
 
