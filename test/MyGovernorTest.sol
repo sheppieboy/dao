fd24f2b744d23d65e192707c8126be851366dade
Luke Sheppard <sheppiegit@gmail.com>
2023-07-18 21:25:10 -0400

    initial test setuo.

---
+++ test/MyGovernorTest.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+import "forge-std/Test.sol";
+import "../src/MyGovernor.sol";
+import "../src/Box.sol";
+import "../src/GovToken.sol";
+import "../src/TimeLock.sol";
+
+contract MyGovernorTest is Test {}
